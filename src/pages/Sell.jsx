1b6b881e4920f877aa90bda893dfca979fc4948b
Manas Khanna <manaskhanna611@gmail.com>
2024-04-25 18:14:21 +0530

    commit

--- src/pages/Sell.jsx
+++ src/pages/Sell.jsx
@@ -1,48 +1,53 @@
-import React, { useState } from 'react';
+import React, {useState } from 'react';
 import axios from 'axios';
 
 function Sell() {
 
   const initialFormData = {
     title: "",
-    year: 0,
+    year: "",
     km: "",
     Fuel: "",
     Transmision: "",
-    Price: 0,
-    Image:""
+    Price: "",
+    Image: ""
   };
 
-
   const [formData, setFormData] = useState(initialFormData);
+
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = async (e) => {
-    console.log("formData", formData);
-   await axios.post(`${process.env.REACT_APP_API_HOST}sell`, formData)
-   setFormData(null)
-      // .then((res) => {
-      //   console.log(res);
-      //   // setFormData({...initialFormData})
-      //  })
-      // .catch((error) => { console.log(error); });
+    // Prevent default form submission behavior
+    e.preventDefault();
+    try {
+      
+      console.log("formData", formData);
+      const response = await axios.post(`${process.env.REACT_APP_API_HOST}sell`, formData);
+      console.log(response); // Handle response from the server
+
+      // Reset form data upon successful submission
+      setFormData(initialFormData);
+      console.log("FORM DATA",formData);
+      
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
   };
 
-  const convertToBase64 = (e)=>{
-    console.log(e);
+  const convertToBase64 = (e) => {
     var reader = new FileReader();
     reader.readAsDataURL(e.target.files[0]);
-    reader.onload=()=>{
+    reader.onload = () => {
       handleChange({ target: { name: "Image", value: reader.result } });
-      console.log(reader.result);
     }
-    reader.onerror=error=>{
-      console.log("ERROR",error);
+    reader.onerror = error => {
+      console.log("ERROR", error);
     }
-  }
+  };
 
   
 
@@ -53,42 +58,42 @@ function Sell() {
           <label className="block text-center text-gray-700 text-sm font-bold mb-2" htmlFor="title">
             Title
           </label>
-          <input onChange={handleChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="title" name="title" type="text" />
+          <input onChange={handleChange} value={formData.title} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="title" name="title" type="text" />
         </div>
 
         <div className="mb-4">
           <label className="block text-center text-gray-700 text-sm font-bold mb-2" htmlFor="year">
             Year
           </label>
-          <input onChange={handleChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="year" name="year" type="number" />
+          <input onChange={handleChange} value={formData.year} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="year" name="year" type="number" />
         </div>
 
         <div className="mb-4">
           <label className="block text-center text-gray-700 text-sm font-bold mb-2" htmlFor="kilometer">
             Km Driven
           </label>
-          <input onChange={handleChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="kilometer" name="km" type="text" />
+          <input onChange={handleChange} value={formData.km} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="kilometer" name="km" type="text" />
         </div>
 
         <div className="mb-4">
           <label className="block text-center text-gray-700 text-sm font-bold mb-2" htmlFor="fuel">
             Fuel Type
           </label>
-          <input onChange={handleChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="fuel" name="Fuel" type="text" />
+          <input onChange={handleChange} value={formData.Fuel} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="fuel" name="Fuel" type="text" />
         </div>
 
         <div className="mb-4">
           <label className="block text-center text-gray-700 text-sm font-bold mb-2" htmlFor="transmission">
             Transmission Type
           </label>
-          <input onChange={handleChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="transmission" name="Transmision" type="text" />
+          <input onChange={handleChange} value={formData.Transmision} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="transmission" name="Transmision" type="text" />
         </div>
 
         <div className="mb-4">
           <label className="block text-center text-gray-700 text-sm font-bold mb-2" htmlFor="price">
             Price
           </label>
-          <input onChange={handleChange} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="price" name="Price" type="number" />
+          <input onChange={handleChange} value={formData.Price} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="price" name="Price" type="number" />
         </div>
 
         <div className="mb-4 text-center">
